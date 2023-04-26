98e31ddc51f2ef261e5e8e8c53ab8bbf5ee5a892
EliasMura <elias-murat@outlook.com>
2023-04-26 10:52:12 -0300

    create HibernateUtil

---
+++ src/main/java/br/com/islink/utils/HibernateUtil.java
@@ -0,0 +1,27 @@
+package br.com.islink.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Exception ex) {
+            System.err.println("ERRO: Erro ao criar SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+    
+}
